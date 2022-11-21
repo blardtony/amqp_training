a3a134822f068c23af8f354dc77fb0d1eb4fea9c
Tony Blard <tonyblard55@gmail.com>
2022-11-21 22:16:08 +0100

    Partie 4 - Consultation des 10 derniers messages

--- src/main/java/fr/lernejo/chat/ChatMessageListener.java
+++ src/main/java/fr/lernejo/chat/ChatMessageListener.java
@@ -5,7 +5,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class ChatMessageListener {
 
+    private final ChatMessageRepository repository;
+
+    public ChatMessageListener(ChatMessageRepository repository) {
+        this.repository = repository;
+    }
+
     public void onMessage(String message) {
-        System.out.println(message);
+        repository.addChatMessage(message);
     }
 }
